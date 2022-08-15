a63d03e5aa2283421442e818c258adb6e0a51496
Kyle Shal <kashal@mun.ca>
2022-08-15 16:17:24 -0230

    page with stuff1

--- process-images.sh
+++ process-images.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-THUMB_OUT_DIR="./assets/auto-thumbnails"
-LARGE_OUT_DIR="./assets/auto-large"
+THUMB_OUT_DIR="./public/assets/auto-thumbnails"
+LARGE_OUT_DIR="./public/assets/auto-large"
 
 mkdir -p "$THUMB_OUT_DIR"
 mkdir -p "$LARGE_OUT_DIR"
 
-for FILE in ./assets/drawings/*;
+for FILE in ./public/assets/drawings/*;
 do
     echo $FILE;
-    FILE_NAME=$(basename "$FILE" png)
+    FILE_NAME=$(basename "$FILE" .png)
 
     convert "$FILE" -define jpeg:size=400x400 -thumbnail 400x400^ -gravity center -extent 400x400 -quality 70% "$THUMB_OUT_DIR/$FILE_NAME.jpg"
-    # convert "$FILE" -resize 1200x1200 "$LARGE_OUT_DIR/$FILE_NAME.png"
+    convert "$FILE" -resize 1200x1200 "$LARGE_OUT_DIR/$FILE_NAME.png"
 done
